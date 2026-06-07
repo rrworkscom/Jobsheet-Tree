a1a4939444f915253a6f06370c3013f712624883
rrworkscom <rrworks010@gmail.com>
2026-06-07 08:57:42 +0700

    Tree - Experiment I | Tree class

---
+++ Week14/Node20.java
@@ -0,0 +1,17 @@
+package Week14;
+
+public class Node20 {
+
+    Student20 data;
+    Node20 left;
+    Node20 right;
+
+    Node20() {
+    }
+
+    Node20(Student20 data) {
+        this.data = data;
+        left = null;
+        left = null;
+    }
+}
