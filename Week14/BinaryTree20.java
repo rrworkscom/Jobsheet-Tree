d5faacb3ce71c0eeed8cd8b61d9fe801f9b49b87
rrworkscom <rrworks010@gmail.com>
2026-06-07 10:02:38 +0700

    Tree - Experiment I | Methods

--- Week14/BinaryTree20.java
+++ Week14/BinaryTree20.java
@@ -38,4 +38,118 @@ public class BinaryTree20 {
             }
         }
     }
+
+    public boolean find(double ipk) {
+        boolean result = false;
+        Node20 current = root;
+        while (current != null) {
+            if (current.data.ipk == ipk) {
+                result = true;
+                break;
+            } else if (ipk < current.data.ipk) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return result;
+    }
+
+    public void traversePreOrder(Node20 node) {
+        if (node != null) {
+            node.data.print();
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    public void traverseInOrder(Node20 node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            node.data.print();
+            traverseInOrder(node.right);
+        }
+    }
+
+    public void traversePostOrder(Node20 node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            node.data.print();
+        }
+    }
+
+    Node20 getSuccessor(Node20 del) {
+        Node20 successor = del.right;
+        Node20 successorParent = del;
+        while (successor.left != null) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+    }
+
+    public void delete(double ipk) {
+        if (isEmpty()) {
+            System.out.println("Tree is empty!");
+            return;
+        }
+        Node20 parent = root;
+        Node20 current = root;
+        boolean isLeftChild = false;
+        while (current.data.ipk != ipk) {
+            parent = current;
+            if (ipk < current.data.ipk) {
+                isLeftChild = true;
+                current = current.left;
+            } else {
+                isLeftChild = false;
+                current = current.right;
+            }
+            if (current == null) {
+                System.out.println("Couldn't find data!");
+                return;
+            }
+        }
+        //delete node with no children 
+        if (current.left == null && current.right == null) {
+            if (current == root) {
+                root = null;
+            } else if (isLeftChild) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        } else if (current.right == null) {//delete node with a left child 
+            if (current == root) {
+                root = current.left;
+            } else if (isLeftChild) {
+                parent.left = current.left;
+            } else {
+                parent.right = current.left;
+            }
+        } else if (current.left == null) {//delete node with a right child 
+            if (current == root) {
+                root = current.right;
+            } else if (isLeftChild) {
+                parent.left = current.right;
+            } else {
+                parent.right = current.right;
+            }
+        } else {//delete node with 2 children 
+            Node20 successor = getSuccessor(current);
+            if (current == root) {
+                root = successor;
+            } else if (isLeftChild) {
+                parent.left = successor;
+            } else {
+                parent.right = successor;
+            }
+            successor.left = current.left;
+        }
+    }
 }
